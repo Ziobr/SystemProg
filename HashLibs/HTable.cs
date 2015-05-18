e40eef308c7c3b75f52183e17e3665d6f9911d06
Ziobr <equus.quagga@gmail.com>
2015-05-19 01:15:33 +0300

    +libs

---
+++ HashLibs/HTable.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemProg
+{
+    public class HTable
+    {
+        //blabla
+        private List<int> Table;
+        public int Size{get; set;}
+
+        public HTable(int size)
+        {
+            Size = size;
+            Table = new List<int>();
+            for (var i = 0; i < size; i++)
+            {
+                Table.Add(0);
+            }
+        }
+
+        public int CalcHash(int key)
+        {
+            return key % Size;
+        }
+
+        public virtual void Insert(int key)
+        {
+            if (Table[CalcHash(key)] == 0)
+            {
+                Console.WriteLine("Inserting key " + key + " to " + CalcHash(key));
+                Table[CalcHash(key)] = key;
+            }
+            else
+            {
+                var c = 3;
+                var i = 0;
+                try
+                {
+                    while (Table[CalcHash(key) + c * i] != 0)
+                    {
+                        Console.WriteLine("Can't insert key " + key + " to " + CalcHash(key + c * i) + " - already taken by " + Table[CalcHash(key + c * i)]);
+                        i++;
+                    }
+                    Console.WriteLine("Inserting key " + key + " to " + CalcHash(key + c * i));
+                    Table[CalcHash(key) + c * i] = key;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Can't insert key " + key + " to table - out of range");
+                }
+            }
+        }
+
+        public virtual void Find(int key)
+        {
+            if (Table[CalcHash(key)] == key)
+            {
+                Console.WriteLine("Key " + key + " found at " + CalcHash(key));
+            }
+            else
+            {
+                var c = 3;
+                var i = 0;
+                try
+                {
+                    while (Table[CalcHash(key) + c * i] != key)
+                    {
+                        //Console.WriteLine("Can't insert key " + key + " to " + CalcHash(key + c * i) + " - already taken by " + Table[CalcHash(key + c * i)]);
+                        i++;
+                    }
+                    Console.WriteLine("Key " + key + " found at " + CalcHash(key) + c * i);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Can't find key " + key);
+                }
+
+            }
+        }
+
+        public override string ToString()
+        {
+            string outString = "";
+            foreach (var h in Table)
+            {
+                outString = outString + h + "\n";
+            }
+            return outString;
+        }
+    }
+}
---
+++ HashLibs/HTableLink.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemProg
+{
+    public class HTableLink : HTable
+    {
+        private List<List<int>> Table;
+        public HTableLink(int size) : base(size)
+        {
+            Size = size;
+            Table = new List<List<int>>(size);
+            for (var i = 0; i < size; i++)
+            {
+                Table.Add(new List<int>());
+            }
+        }
+
+        public override void Insert(int key)
+        {
+            if (!Table[CalcHash(key)].Contains(key))
+            {
+                Console.WriteLine("Inserting key " + key + " to " + CalcHash(key));
+                Table[CalcHash(key)].Add(key);
+            }
+        }
+
+        public override void Find(int key)
+        {
+            if (Table[CalcHash(key)].Contains(key))
+            {
+                Console.WriteLine("Key " + key + " found at " + CalcHash(key) + ", " + Table[CalcHash(key)].IndexOf(key));
+            }
+            else
+            {
+                Console.WriteLine("Can't find key " + key);
+            }
+        }
+
+        public override string ToString()
+        {
+            string outString = "";
+            foreach (var h in Table)
+            {
+                var line = "[";
+                foreach (var item in h)
+                {
+                    line = line + item + ',';
+                }
+                if (line[line.Length - 1] == ',')
+                    line = line.Remove(line.Length - 1);
+                outString = outString + line + "]\n";
+            }
+            return outString;
+        }
+
+    }
+}
