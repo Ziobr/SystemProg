f9d53d45148dac0b1b9f04b01cf47be6de5de7d8
Ziobr <equus.quagga@gmail.com>
2015-05-19 01:08:05 +0300

    SystemProg lab #5

---
+++ HashTable2/HashProgram2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemProg
+{
+    class HashProgram2
+    {
+        static void Main(string[] args)
+        {
+            // Let M = 18
+            // Init hash table
+            HTableLink HashTable = new HTableLink(18);
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Hash Table:");
+            Console.WriteLine(HashTable.ToString());
+            Console.WriteLine("-------------------------------------");
+
+            // N = 18
+            var keys = new int[18];
+            Random rnd = new Random();
+            for (var i = 0; i < 18; i++)
+            {
+                keys[i] = rnd.Next(32768);
+            }
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Keys are: ");
+            foreach (var key in keys) Console.WriteLine(key);
+            Console.WriteLine("-------------------------------------");
+            Console.ReadLine();
+
+
+            foreach (var key in keys)
+            {
+                HashTable.Insert(key);
+            }
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Hash Table:");
+            Console.WriteLine(HashTable.ToString());
+            Console.WriteLine("-------------------------------------");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                int inInt;
+                try
+                {
+                    inInt = Convert.ToInt32(input);
+                    HashTable.Find(inInt);
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect number");
+                }
+            }
+        }
+    }
+}
---
+++ SystemProg/HashProgram1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemProg
+{
+    class HashProgram1
+    {
+        static void Main(string[] args)
+        {
+            // Let M = 31
+            // Init hash table
+            HTable HashTable = new HTable(31);
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Hash Table:");
+            Console.WriteLine(HashTable.ToString());
+            Console.WriteLine("-------------------------------------");
+
+            // N = 18
+            var keys = new int[18];
+            Random rnd = new Random();
+            for (var i = 0; i < 18; i++)
+            {
+                keys[i] = rnd.Next(32768);
+            }
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Keys are: ");
+            foreach (var key in keys) Console.WriteLine(key);
+            Console.WriteLine("-------------------------------------");
+            Console.ReadLine();
+
+
+            foreach (var key in keys)
+            {
+                HashTable.Insert(key);
+            }
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Hash Table:");
+            Console.WriteLine(HashTable.ToString());
+            Console.WriteLine("-------------------------------------");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                int inInt;
+                try
+                {
+                    inInt = Convert.ToInt32(input);
+                    HashTable.Find(inInt);
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect number");
+                }
+            }
+        }
+    }
+}
